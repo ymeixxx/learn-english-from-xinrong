1c4efacfae016030b5f997438973582eb67598be
ymeixxx <ymeixxx@users.noreply.github.com>
2025-08-28 09:25:36 +0000

    deploy: 8ada63853c0bf3bca437827b0c551491e8a2016b

---
+++ assets/courses_lesson-29.md.BtT29bCm.lean.js
@@ -0,0 +1,59 @@
+import{D as n,c as r,I as h,j as t,a,a6 as i,o as s}from"./chunks/framework.CIJOd5-K.js";const k=t("h1",{id:"lesson-29",tabindex:"-1"},[a("Lesson-29 "),t("a",{class:"header-anchor",href:"#lesson-29","aria-label":'Permalink to "Lesson-29"'},"​")],-1),o=t("p",null,"儘衇蝠騕眧駘 관㕈熝㳔蝠騕 (睘  騦)",-1),w=t("p",null,"Hello 你好，我是星荣，欢迎来到零基础学英语的第 29 课，第二阶 段的 19 课。 今天，我们先来总结一个非常重要的词，be 的用法。 首先，你需要清楚，这个词和我们学的平常的动词不太一样，普通 的动词比如 do 这个动作词，跟在不同的人称后面要用不同的形式，比 如第一人称第二人称以及第三人称的复数形式都用它的原型 do，只有 第三人称的单数形式要用 does，如果这个动作发生在过去，那么这个 动词就用它的过去式的形式 did。所以，在不考虑时间的情况下，do 这 个词的规律就是第三人称单数后面用 does，其他的情况都用原型 do。 如果需要使用不定式的形式，直接在动词原型 do 的前面添加 to 变成 to do 就可以了。 而 be 这个词规律是不同的，第一人称 I 用 am，第二人称 You 用 are，第三人称单数 He She It Xingrong 用 is，复数 They 用 are，如果想 要表达过去，那么 am 和 is 都要变成 was，are 会变成 were。下面是重 点了，如果想要用这个词的不定式的形式，应该在哪个词前面添加 to 呢？其实这五个词都不对，因为它们都不是原型，原型就是 be，所以 如果想要使用不定式，to 后面添加的就是 be，这个 be 就是 am is are was 和 were 的原型，to be 就是不定式的形式。",-1),l=t("p",null,"所以，我们总结的结果就是，普通的动词，就是表示某一个特定动 作的动词，比如 do，eat 等，用在非第三人称单数后面都是动词原形， 这个动词原形和用在 to 后面构成不定式的那个词是一样的。 而表示赋值含义的这五个词，和刚刚说的动词不同，它们只是根据 不同的人称和时间来选择使用不同的词，它们五个都不是原型，原型是 另外的 be 这个词，所以它的不定式的表达方式是 to be，和这些人称词 后面所跟的词都是不一样的。 微信：xingrong-english 公众号：Hi 要大声说出来",-1),g=i("",1),p=JSON.parse('{"title":"Lesson-29","description":"","frontmatter":{},"headers":[],"relativePath":"courses/lesson-29.md","filePath":"courses/lesson-29.md","lastUpdated":null}'),z={name:"courses/lesson-29.md"},u=Object.assign(z,{setup(c){const d=[{zh:"这里",en:"here",kk:"/hɪr/"},{zh:"我",en:"I",kk:"/aɪ/"},{zh:"(I，现在时)be",en:"am",kk:"/æm/"},{zh:"我在这里",en:"I am here",kk:"/aɪ/ /æm/ /hɪr/"},{zh:"想要",en:"want",kk:"/wɑnt/"},{zh:"我在这里",en:"I am here",kk:"/aɪ/ /æm/ /hɪr/"},{zh:"我想要在这里",en:"I want to be here",kk:"/aɪ/ /wɑnt/ /tə/ /bi/ /hɪr/"},{zh:"不",en:"don't",kk:"/dont/"},{zh:"我不想在这里",en:"I don't want to be here",kk:"/aɪ/ /dont/ /wɑnt/ /tə/ /bi/ /hɪr/"},{zh:"那里",en:"there",kk:"/ðɛr/"},{zh:"我不想去那里",en:"I don't want to be there",kk:"/aɪ/ /dont/ /wɑnt/ /tə/ /bi/ /ðɛr/"},{zh:"我想要去那里",en:"I want to be there",kk:"/aɪ/ /wɑnt/ /tə/ /bi/ /ðɛr/"},{zh:"我不想在这里，我想要去那里",en:`I don't want to be here, I want to
+be there`,kk:`/aɪ/ /dont/ /wɑnt/ /tə/ /bi/ /hɪr/ /aɪ/ /wɑnt/
+/tə/ /bi/ /ðɛr/`},{zh:"我在这里",en:"I am here",kk:"/aɪ/ /æm/ /hɪr/"},{zh:"当……时候",en:"when",kk:"/wɛn/"},{zh:"当我在这里的时候",en:"when I am here",kk:"/wɛn/ /aɪ/ /æm/ /hɪr/"},{zh:"我想要去那里",en:"I want to be there",kk:"/aɪ/ /wɑnt/ /tə/ /bi/ /ðɛr/"},{zh:`当我在这里的时候，我想要去
+那里`,en:`when I am here, I want to be
+there`,kk:`/wɛn/ /aɪ/ /æm/ /hɪr/ /aɪ/ /wɑnt/ /tə/ /bi/
+/ðɛr/`},{zh:`当我(过去)在这里的时候，我
+(过去)想要去那里`,en:`when I was here, I wanted to be
+there`,kk:`/wɛn/ /aɪ/ /wəz/ /hɪr/ /aɪ/ /wɑntɪd/ /tə/ /bi/
+/ðɛr/`},{zh:"老师",en:"teacher",kk:"/'titʃɚ/"},{zh:"我是一名老师",en:"I am a teacher",kk:"/aɪ/ /æm/ /ə/ /'titʃɚ/"},{zh:"想要",en:"want",kk:"/wɑnt/"},{zh:"我想要成为一名老师",en:"I want to be a teacher",kk:"/aɪ/ /wɑnt/ /tə/ /bi/ /ə/ /'titʃɚ/"},{zh:"富有的",en:"rich",kk:"/rɪtʃ/"},{zh:"我是富有的",en:"I am rich",kk:"/aɪ/ /æm/ /rɪtʃ/"},{zh:"想要",en:"want",kk:"/wɑnt/"},{zh:"我想要变得富有",en:"I want to be rich",kk:"/aɪ/ /wɑnt/ /tə/ /bi/ /rɪtʃ/"},{zh:"我在这里",en:"I am here",kk:"/aɪ/ /æm/ /hɪr/"},{zh:"(I，过去时)be",en:"was",kk:"/wəz/"},{zh:"我(过去)在这里",en:"I was here",kk:"/aɪ/ /wəz/ /hɪr/"},{zh:"当……时候",en:"when",kk:"/wɛn/"},{zh:"当我(过去)在这里的时候",en:"when I was here",kk:"/wɛn/ /aɪ/ /wəz/ /hɪr/"},{zh:"在……之前；以前",en:"before",kk:"/bɪ'fɔr/"},{zh:"当我以前在这里的时候",en:"when I was here before",kk:"/wɛn/ /aɪ/ /wəz/ /hɪr/ /bɪ'fɔr/"},{zh:"注意到；察觉到",en:"to notice",kk:"/tə/ /ˈnotɪs/"},{zh:"我",en:"I",kk:"/aɪ/"},{zh:"我注意到",en:"I notice",kk:"/aɪ/ /ˈnotɪs/"},{zh:"那个",en:"that",kk:"/ðæt/"},{zh:"我注意到那个",en:"I notice that",kk:"/aɪ/ /ˈnotɪs/ /ðæt/"},{zh:"(过去)注意到",en:"noticed",kk:"/ˈnotɪst/"},{zh:"我(过去)注意到那个",en:"I noticed that",kk:"/aɪ/ /ˈnotɪst/ /ðæt/"},{zh:"(过去)没有",en:"didn't",kk:"/ˈdɪdnt/"},{zh:"我(过去)没有注意到那个",en:"I didn't notice that",kk:"/aɪ/ /ˈdɪdnt/ /ˈnotɪs/ /ðæt/"},{zh:"当我以前在这里的时候",en:"when I was here before",kk:"/wɛn/ /aɪ/ /wəz/ /hɪr/ /bɪ'fɔr/"},{zh:`我(过去)没有注意那个当我以
+前在这里的时候`,en:`I didn't notice that when I was
+here before`,kk:`/aɪ/ /ˈdɪdnt/ /ˈnotɪs/ /ðæt/ /wɛn/ /aɪ/
+/wəz/ /hɪr/ /bɪ'fɔr/`},{zh:"我(过去)注意到",en:"I noticed",kk:"/aɪ/ /ˈnotɪst/"},{zh:"某个东西",en:"something",kk:"/'sʌmθɪŋ/"},{zh:"错误的",en:"wrong",kk:"/rɔŋ/"},{zh:"错误的某个东西",en:"something wrong",kk:"/'sʌmθɪŋ/ /rɔŋ/"},{zh:"那里",en:"there",kk:"/ðɛr/"},{zh:"错误的某个东西在那里",en:"something wrong is there",kk:"/'sʌmθɪŋ/ /rɔŋ/ /ɪz/ /ðɛr/"},{zh:"存在",en:"there is",kk:"/ðɛr/ /ɪz/"},{zh:"存在错误的某个东西",en:"there is something wrong",kk:"/ðɛr/ /ɪz/ /'sʌmθɪŋ/ /rɔŋ/"},{zh:"这里",en:"here",kk:"/hɪr/"},{zh:"这里有错误的某个东西",en:"there is something wrong here",kk:"/ðɛr/ /ɪz/ /'sʌmθɪŋ/ /rɔŋ/ /hɪr/"},{zh:"(过去)存在",en:"there was",kk:"/ðɛr/ /wəz/"},{zh:"这里(过去)有错误的某个东西",en:`there was something wrong
+here`,kk:"/ðɛr/ /wəz/ /'sʌmθɪŋ/ /rɔŋ/ /hɪr/"},{zh:"我(过去)注意到",en:"I noticed",kk:"/aɪ/ /ˈnotɪst/"},{zh:`我(过去)注意到这里(过去)有错
+误的某个东西`,en:`I noticed there was something
+wrong here`,kk:`/aɪ/ /ˈnotɪst/ /ðɛr/ /wəz/ /'sʌmθɪŋ/ /rɔŋ/
+/hɪr/`},{zh:"那个",en:"that",kk:"/ðæt/"},{zh:`我(过去)注意到(那个)这里(过
+去)有错误的某个东西`,en:`I noticed that there was
+something wrong here`,kk:`/aɪ/ /ˈnotɪst/ /ðæt/ /ðɛr/ /wəz/ /'sʌmθɪŋ/
+/rɔŋ/ /hɪr/`},{zh:"不同的",en:"different",kk:"/'dɪfrənt/"},{zh:"某个东西",en:"something",kk:"/'sʌmθɪŋ/"},{zh:"不同的某个东西",en:"something different",kk:"/'sʌmθɪŋ/ /'dɪfrənt/"},{zh:"存在不同的某个东西",en:"there is something different",kk:"/ðɛr/ /ɪz/ /'sʌmθɪŋ/ /'dɪfrənt/"},{zh:"这里",en:"here",kk:"/hɪr/"},{zh:"这里有不同的某个东西",en:`there is something different
+here`,kk:"/ðɛr/ /ɪz/ /'sʌmθɪŋ/ /'dɪfrənt/ /hɪr/"},{zh:"(过去)存在",en:"there was",kk:"/ðɛr/ /wəz/"},{zh:"这里(过去)有不同的某个东西",en:`there was something different
+here`,kk:"/ðɛr/ /wəz/ /'sʌmθɪŋ/ /'dɪfrənt/ /hɪr/"},{zh:"我(过去)注意到",en:"I noticed",kk:"/aɪ/ /ˈnotɪst/"},{zh:`我(过去)注意到这里(过去)存在
+不同的某个东西`,en:`I noticed there was something
+different here`,kk:`/aɪ/ /ˈnotɪst/ /ðɛr/ /wəz/ /'sʌmθɪŋ/
+/'dɪfrənt/ /hɪr/`},{zh:"那个",en:"that",kk:"/ðæt/"},{zh:`我(过去)注意到(那个)这里(过
+去)存在不同的某个东西`,en:`I noticed that there was
+something different here`,kk:`/aɪ/ /ˈnotɪst/ /ðæt/ /ðɛr/ /wəz/ /'sʌmθɪŋ/
+/'dɪfrənt/ /hɪr/`},{zh:"不同的",en:"different",kk:"/'dɪfrənt/"},{zh:"不同之处",en:"difference",kk:"/'dɪfrəns/"},{zh:"在……之间",en:"between",kk:"/bɪ'twin/"},{zh:"和",en:"and",kk:"/ənd/"},{zh:"你和我",en:"you and me",kk:"/ju/ /ənd/ /mi/"},{zh:"在你和我之间",en:"between you and me",kk:"/bɪ'twin/ /ju/ /ənd/ /mi/"},{zh:"在你和我之间的不同",en:"difference between you and me",kk:"/'dɪfrəns/ /bɪ'twin/ /ju/ /ənd/ /mi/"},{zh:"大的",en:"big",kk:"/bɪɡ/"},{zh:"一个大的不同",en:"a big difference",kk:"/ə/ /bɪɡ/ /'dɪfrəns/"},{zh:"在你和我之间的一个大的不同",en:`a big difference between you
+and me`,kk:"/ə/ /bɪɡ/ /'dɪfrəns/ /bɪ'twin/ /ju/ /ənd/ /mi/"},{zh:"那里",en:"there",kk:"/ðɛr/"},{zh:`在你和我之间的一个大的不同
+在那里`,en:`a big difference between you
+and me is there`,kk:`/ə/ /bɪɡ/ /'dɪfrəns/ /bɪ'twin/ /ju/ /ənd/ /mi/
+/ɪz/ /ðɛr/`},{zh:"存在",en:"there is",kk:"/ðɛr/ /ɪz/"},{zh:`存在在你和我之间的一个大的
+不同`,en:`there is a big difference
+between you and me`,kk:`/ðɛr/ /ɪz/ /ə/ /bɪɡ/ /'dɪfrəns/ /bɪ'twin/ /ju/
+/ənd/ /mi/`},{zh:"武器",en:"weapon",kk:"/ˈwɛpən/"},{zh:"工具",en:"tool",kk:"/tul/"},{zh:"一个武器和一个工具",en:"a weapon and a tool",kk:"/ə/ /ˈwɛpən/ /ənd/ /ə/ /tul/"},{zh:"在……之间",en:"between",kk:"/bɪ'twin/"},{zh:"在一个武器和一个工具之间",en:"between a weapon and a tool",kk:"/bɪ'twin/ /ə/ /ˈwɛpən/ /ənd/ /ə/ /tul/"},{zh:"存在一个大的不同",en:"there is a big difference",kk:"/ðɛr/ /ɪz/ /ə/ /bɪɡ/ /'dɪfrəns/"},{zh:`存在在一个武器和一个工具之
+间的一个大的不同`,en:`there is a big difference
+between a weapon and a tool`,kk:`/ðɛr/ /ɪz/ /ə/ /bɪɡ/ /'dɪfrəns/ /bɪ'twin/ /ə/
+/ˈwɛpən/ /ənd/ /ə/ /tul/`},{zh:"正确",en:"right",kk:"/raɪt/"},{zh:"错误",en:"wrong",kk:"/rɔŋ/"},{zh:"正确和错误",en:"right and wrong",kk:"/raɪt/ /ənd/ /rɔŋ/"},{zh:"在……之间",en:"between",kk:"/bɪ'twin/"},{zh:"在对和错之间",en:"between right and wrong",kk:"/bɪ'twin/ /raɪt/ /ənd/ /rɔŋ/"},{zh:"不同之处",en:"difference",kk:"/'dɪfrəns/"},{zh:"那个不同之处(区别)",en:"the difference",kk:"/ðə/ /'dɪfrəns/"},{zh:"在对和错之间的区别",en:`the difference between right
+and wrong`,kk:"/ðə/ /'dɪfrəns/ /bɪ'twin/ /raɪt/ /ənd/ /rɔŋ/"},{zh:"知道",en:"to know",kk:"/tə/ /no/"},{zh:"知道对与错的区别",en:`to know the difference between
+right and wrong`,kk:`/tə/ /no/ /ðə/ /'dɪfrəns/ /bɪ'twin/ /raɪt/ /ənd/
+/rɔŋ/`},{zh:"我们",en:"we",kk:"/wi/"},{zh:"我们知道",en:"we know",kk:"/wi/ /no/"},{zh:"所有；全部",en:"all",kk:"/ɔl/"},{zh:"我们都知道",en:"we all know",kk:"/wi/ /ɔl/ /no/"},{zh:"我们都知道对与错的区别",en:`we all know the difference
+between right and wrong`,kk:`/wi/ /ɔl/ /no/ /ðə/ /'dɪfrəns/ /bɪ'twin/ /raɪt/
+/ənd/ /rɔŋ/`},{zh:"你必须知道",en:"you have to know",kk:"/ju/ /hæv/ /tə/ /no/"},{zh:"你必须知道对与错的区别",en:`you have to know the difference
+between right and wrong`,kk:`/ju/ /hæv/ /tə/ /no/ /ðə/ /'dɪfrəns/ /bɪ'twin/
+/raɪt/ /ənd/ /rɔŋ/`},{zh:"告诉",en:"to tell",kk:"/tə/ /tɛl/"},{zh:"区别",en:"the difference",kk:"/ðə/ /'dɪfrəns/"},{zh:"分辨区别",en:"to tell the difference",kk:"/tə/ /tɛl/ /ðə/ /'dɪfrəns/"},{zh:"在……之间",en:"between",kk:"/bɪ'twin/"},{zh:"正确和错误",en:"right and wrong",kk:"/raɪt/ /ənd/ /rɔŋ/"},{zh:"在对和错之间",en:"between right and wrong",kk:"/bɪ'twin/ /raɪt/ /ənd/ /rɔŋ/"},{zh:"对与错的区别",en:`the difference between right
+and wrong`,kk:"/ðə/ /'dɪfrəns/ /bɪ'twin/ /raɪt/ /ənd/ /rɔŋ/"},{zh:"分辨是非",en:`to tell the difference between
+right and wrong`,kk:`/tə/ /tɛl/ /ðə/ /'dɪfrəns/ /bɪ'twin/ /raɪt/ /ənd/
+/rɔŋ/`},{zh:"年龄大的；老的",en:"old",kk:"/old/"},{zh:"足够",en:"enough",kk:"/ɪ'nʌf/"},{zh:"足够年龄大",en:"old enough",kk:"/old/ /ɪ'nʌf/"},{zh:"你是",en:"you are",kk:"/ju/ /ɚ/"},{zh:"你是足够年龄大的",en:"you are old enough",kk:"/ju/ /ɚ/ /old/ /ɪ'nʌf/"},{zh:"你是年龄大的足够分辨是非",en:`you are old enough to tell the
+difference between right and
+wrong`,kk:`/ju/ /ɚ/ /old/ /ɪ'nʌf/ /tə/ /tɛl/ /ðə/ /'dɪfrəns/
+/bɪ'twin/ /raɪt/ /ənd/ /rɔŋ/`},{zh:"孩子",en:"child",kk:"/tʃaɪld/"},{zh:"孩子们",en:"children",kk:"/'tʃɪldrən/"},{zh:"我们的",en:"our",kk:"/'aʊɚ/"},{zh:"我们的孩子们",en:"our children",kk:"/'aʊɚ/ /'tʃɪldrən/"},{zh:"教",en:"to teach",kk:"/tə/ /titʃ/"},{zh:"我们需要",en:"we need",kk:"/wi/ /nid/"},{zh:"我们需要教",en:"we need to teach",kk:"/wi/ /nid/ /tə/ /titʃ/"},{zh:"我们需要教我们的孩子们",en:"we need to teach our children",kk:"/wi/ /nid/ /tə/ /titʃ/ /'aʊɚ/ /'tʃɪldrən/"},{zh:"分辨区别",en:"to tell the difference",kk:"/tə/ /tɛl/ /ðə/ /'dɪfrəns/"},{zh:"在对和错之间",en:"between right and wrong",kk:"/bɪ'twin/ /raɪt/ /ənd/ /rɔŋ/"},{zh:"辨别是非",en:`to tell the difference between
+right and wrong`,kk:`/tə/ /tɛl/ /ðə/ /'dɪfrəns/ /bɪ'twin/ /raɪt/ /ənd/
+/rɔŋ/`},{zh:"如何",en:"how",kk:"/haʊ/"},{zh:"如何辨别是非",en:`how to tell the difference
+between right and wrong`,kk:`/haʊ/ /tə/ /tɛl/ /ðə/ /'dɪfrəns/ /bɪ'twin/ /raɪt/
+/ənd/ /rɔŋ/`},{zh:`我们需要教我们的孩子们如何
+辨别是非`,en:`we need to teach our children
+how to tell the difference
+between right and wrong`,kk:`/wi/ /nid/ /tə/ /titʃ/ /'aʊɚ/ /'tʃɪldrən/ /haʊ/
+/tə/ /tɛl/ /ðə/ /'dɪfrəns/ /bɪ'twin/ /raɪt/ /ənd/
+/rɔŋ/`}];return(f,b)=>{const e=n("StatementGroup");return s(),r("div",null,[k,o,w,l,h(e,{data:d}),g])}}});export{p as __pageData,u as default};
